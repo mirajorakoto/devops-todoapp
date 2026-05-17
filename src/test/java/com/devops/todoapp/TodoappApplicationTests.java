8651f6a8efd0458311947f3c05625fae0bc38106
mirajorakoto <mirajohenry@gmail.com>
2026-05-17 09:56:25 +0300

    fix: corriger pipeline et tests CI/CD

--- src/test/java/com/devops/todoapp/TodoappApplicationTests.java
+++ src/test/java/com/devops/todoapp/TodoappApplicationTests.java
@@ -1,11 +1,13 @@
 package com.devops.todoapp;
 
 import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class TodoappApplicationTests {
 
     @Test
     void contextLoads() {
-        System.out.println("✅ Test OK !");
+        System.out.println("Test OK !");
+        assertTrue(true);
     }
 }
